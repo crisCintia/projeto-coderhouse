d6f4d966d1fb473726065846dc142b6a5a01518f
ChristianBDevezas <christianbdevezas@hotmail.com>
2023-02-21 15:10:26 -0300

    update shop script

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,18 +1,26 @@
-const itemValue = document.querySelector(".product__price__item");
+// const itemValue = document.querySelector(".product__price__item");
+const itemValue = document.querySelector(".product__price__item").innerText;
 const itemQuantity = document.querySelector(".cart__buttons__quantity");
 const totalValue = document.querySelector(".cart__price_total");
 const subtractBtn = document.querySelector(".cart__buttons__subtract");
 const addBtn = document.querySelector(".cart__buttons__add");
 const removeBtn = document.querySelector(".cart__buttons__remove");
 const container = document.querySelector(".cart__description");
+
 let counter = 1;
 let removedContainer = false;
+let change = Number(itemValue.replace(',', '.'));
 
 const calculate = () => {
     itemQuantity.textContent = counter;
-    let total = itemValue.textContent * itemQuantity.textContent;
-    total = Number(total);
-    totalValue.textContent = total.toFixed(2);
+    // let total = itemValue.textContent * itemQuantity.textContent;
+    // total = Number(total);
+    // totalValue.textContent = total.toFixed(2);
+
+    // let mult = change * itemQuantity.textContent;
+    let mult = change * counter;
+    let totalField = mult.toFixed(2).toString().replace('.', ',');
+    totalValue.textContent = totalField;
 }
 
 subtractBtn.addEventListener("click", () => {
