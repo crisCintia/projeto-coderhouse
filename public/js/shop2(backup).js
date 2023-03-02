dcdeae0a8c5583bcb1b967eb889ba206a1b61331
ChristianBDevezas <christianbdevezas@hotmail.com>
2023-03-02 14:04:10 -0300

    update shop2 page

--- public/js/shop2(backup).js
+++ public/js/shop2(backup).js
@@ -6,6 +6,7 @@ let name = prompt("Qual o nome do vinho?");
 let price = prompt("Qual o valor do vinho?");
 let quantity = prompt("Qual a quantidade de vinhos?");
 
+// Class
 class product {
     constructor(name, price, quantity) {
         this.name = name;
@@ -16,6 +17,7 @@ class product {
     printTotalValue() {
         let change = Number(price.replace(',', '.'));
         wineName.innerHTML = this.name;
+        // winePrice.innerHTML = this.price;
         winePrice.innerHTML = change.toFixed(2).toString().replace('.', ',');
 
         let mult = change * this.quantity;
@@ -25,22 +27,38 @@ class product {
 }
 
 const wine1 = new product(name, price, quantity);
-// console.log(wine1);
+console.log(wine1);
 wine1.printTotalValue();
 
-// Array contendo os produtos
-const products = [];
-products.push(wine1);
-products.push(new product("Matetic Corralillo", "75,50", 2));
-products.push(new product("Vistamar Brisa", "85,80", 3));
-products.push(new product("Rio Flor Douro", "80,50", 2));
-// console.log(products);
+// Constructor Function
+// function attributes(name, price, quantity) {
+//     let change = Number(price.replace(',', '.'));
 
-for(const product of products) {
-    console.log(product);
-}
+//     this.name = name;
+//     this.price = price;
+//     this.quantity = quantity;
+//     this.printTotalValue = function() {        
+//         wineName.innerHTML = this.name;
+//         // winePrice.innerHTML = this.price;
+//         winePrice.innerHTML = change.toFixed(2).toString().replace('.', ',');
 
-// Código para indicar o valor total dos vinhos, conforme o usuário for diminuindo ou aumentando a quantidade
+//         let mult = change * this.quantity;
+//         let totalField = mult.toFixed(2).toString().replace('.', ',');
+//         totalPrice.textContent = totalField;
+//     }
+// }
+
+// // const wine1 = new attributes("Barone Montalto", "77,80", 3);
+// const wine1 = new attributes(name, price, quantity);
+// console.log(wine1);
+// wine1.printTotalValue();
+
+// const wine2 = new attributes("Matetic Corralillo", "75,50", 5);
+// const wine2 = new attributes(name, price, quantity);
+// console.log(wine2);
+// wine2.printTotalValue();
+
+// const itemValue = document.querySelector(".product__price__item");
 const itemValue = document.querySelector(".product__price__item").innerText;
 const itemQuantity = document.querySelector(".cart__buttons__quantity");
 const totalValue = document.querySelector(".cart__price_total");
@@ -53,10 +71,16 @@ const container = document.querySelector(".cart__description");
 let counter = quantity;
 let removedContainer = false;
 let change = Number(itemValue.replace(',', '.'));
+
 itemQuantity.textContent = quantity;
 
 const calculate = () => {
     itemQuantity.textContent = counter;
+    // let total = itemValue.textContent * itemQuantity.textContent;
+    // total = Number(total);
+    // totalValue.textContent = total.toFixed(2);
+
+    // let mult = change * itemQuantity.textContent;
     let mult = change * counter;
     let totalField = mult.toFixed(2).toString().replace('.', ',');
     totalValue.textContent = totalField;
